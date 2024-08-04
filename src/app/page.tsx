619e667340f838287818132f2837025a7a286955
VictorFil67 <victorfilinovych@gmail.com>
2024-08-04 20:30:02 +0300

    render server and client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import StatusLabel, { Status } from '@/app/components/status-label';
 import { headers } from 'next/headers';
 import AddCompanyButton from '@/app/components/add-company-button';
+import ServerComponent from '@/app/components/server-component';
+import ClientComponent from '@/app/components/client-component';
+import ServerComponentCopy from '@/app/components/server-component copy';
 
 export default function Home() {
-  console.log(headers())
+  console.log(headers());
   return (
     <main>
       <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
@@ -11,7 +14,11 @@ export default function Home() {
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
       <StatusLabel status={Status.Suspended}>Suspended</StatusLabel> */}
-      <AddCompanyButton/>
+      <AddCompanyButton />
+      <ServerComponent />
+      <ClientComponent>
+        <ServerComponentCopy />
+      </ClientComponent>
     </main>
   );
 }
