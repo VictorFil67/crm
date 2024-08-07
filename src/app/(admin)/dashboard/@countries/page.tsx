007cfd6e8c918823bdc6ac0427f5385f01be8c1f
VictorFil67 <victorfilinovych@gmail.com>
2024-08-07 21:16:06 +0300

    update promotions again

--- src/app/(admin)/dashboard/@countries/page.tsx
+++ src/app/(admin)/dashboard/@countries/page.tsx
@@ -1,34 +1,34 @@
 import React from 'react';
-import { getPromotions } from '@/lib/api';
-import SummaryTable from '@/app/components/summary-table';
-import SummaryTableHeader from '@/app/components/summary-table-header';
-import SummaryTableCell from '@/app/components/summary-table-cell';
+import Image from 'next/image';
+import clsx from 'clsx';
+import { getCompanies, getCountries } from '@/lib/api';
 import DashboardCard from '@/app/components/dashboard-card';
+import getCountById from '@/lib/utils/getCountById';
 
 export interface PageProps {}
 
 export default async function Page({}: PageProps) {
-  const data = await getPromotions();
+  const countries = await getCountries();
+  const companies = await getCompanies();
+
+  const counts = getCountById(companies, 'countryId');
 
   return (
-    <DashboardCard label="Promotions">
-      <SummaryTable
-        headers={
-          <>
-            <SummaryTableHeader>Company</SummaryTableHeader>
-            <SummaryTableHeader>Name</SummaryTableHeader>
-            <SummaryTableHeader align="center">%</SummaryTableHeader>
-          </>
-        }
-      >
-        {data.map(({ id, title, companyTitle, discount }) => (
-          <tr key={id}>
-            <SummaryTableCell>{companyTitle}</SummaryTableCell>
-            <SummaryTableCell>{title}</SummaryTableCell>
-            <SummaryTableCell align="center">{`-${discount}%`}</SummaryTableCell>
-          </tr>
-        ))}
-      </SummaryTable>
+    <DashboardCard label="Countries of companies">
+      <div className="flex items-end pb-5 px-5 gap-2">
+        <div>
+          {countries.map(({ id, title }) => (
+            <p
+              key={id}
+              className={clsx(
+                'text-sm text-gray-900 font-medium',
+                'before:inline-block before:w-2 before:h-2 before:rounded-full before:align-middle before:mr-2 before:bg-purple-200',
+              )}
+            >{`${title} - ${counts[id] || 0}`}</p>
+          ))}
+        </div>
+        <Image width={395} height={262} src="/images/world.svg" alt="world" />
+      </div>
     </DashboardCard>
   );
 }
