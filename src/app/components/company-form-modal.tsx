5df4cc405010eba1ebbe3d5c02cd302540db27f0
VictorFil67 <victorfilinovych@gmail.com>
2024-08-12 22:45:01 +0300

    updated

--- src/app/components/company-form-modal.tsx
+++ src/app/components/company-form-modal.tsx
@@ -11,24 +11,3 @@ export default function CompanyFormModal({ onClose, ...rest }: ModalProps) {
     </Modal>
   );
 }
-
-// 'use client';
-
-// import React from 'react';
-// import CompanyForm, { CompanyFormProps } from '@/app/components/company-form';
-// import Modal, { ModalProps } from '@/app/components/modal';
-
-// export interface CompanyFormModalProps extends ModalProps {
-//   onSubmit: CompanyFormProps['onSubmit'];
-// }
-
-// export default function CompanyFormModal({
-//   onSubmit,
-//   ...rest
-// }: CompanyFormModalProps) {
-//   return (
-//     <Modal {...rest}>
-//       <CompanyForm onSubmit={onSubmit} />
-//     </Modal>
-//   );
-// }
