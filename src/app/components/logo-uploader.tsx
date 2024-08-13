87483a6f8504f3a60b8f90ca1a6b728b8b47ae88
VictorFil67 <victorfilinovych@gmail.com>
2024-08-13 23:10:10 +0300

    update input-field

--- src/app/components/logo-uploader.tsx
+++ src/app/components/logo-uploader.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 export interface LogoUploaderProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'> {
   label?: string;
+  square?: boolean;
 }
 
 export default function LogoUploader({
