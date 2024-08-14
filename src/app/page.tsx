616b736df3ee5af28bd842c7f95d11f0162b5d71
VictorFil67 <victorfilinovych@gmail.com>
2024-08-14 13:50:09 +0300

    apdated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      {/* <AddCompanyButton />
-      <MagicButton /> */}
     </main>
   );
 }
